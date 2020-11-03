85a8a696598010a44791722d99585c7879549af3
Hunter Krieg <kriegster108@gmail.com>
2020-11-02 22:26:50 -0700

    added mail support

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
 import RequestModule from "./requests/request_module.js";
 import Logic from "./requests/logic.js";
 import pkg from "datejs";
+import Mail from "./requests/mail.js";
+
 const request = new RequestModule();
 const logic = new Logic();
+const mail = new Mail();
 
 async function app() {
   const futureDate = logic.getNextWeekday("wednesday");
@@ -11,6 +14,7 @@ async function app() {
     wait(60000 * 15);
   } else {
     console.log(`opening found for ${futureDate} !!!!`);
+    await mail.sendEmail();
     wait(60000 * 15);
   }
 }
---
+++ app/requests/mail.js
@@ -0,0 +1,30 @@
+import pkg from "nodemailer";
+// async..await is not allowed in global scope, must use a wrapper
+
+export default class Mail {
+  async sendEmail() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = pkg.createTransport({
+      host: "email-smtp.us-west-2.amazonaws.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "", // generated ethereal user
+        pass: "", // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Hunter Krieg 👻" <kriegster108@gmail.com>', // sender address
+      to: "kriegster108@gmail.com, kriegster108@gmail.com", // list of receivers
+      subject: "Reservation available at Englewood Location", // Subject line
+      html:
+        "Hey There!<br>There is a reservation available at Englewood Location for this upcoming Wednesday! Book us now!!!!! <br> Love you babe: Hunty <br><br><b>Reservation Link:</b> https://app.rockgympro.com/b/widget/?a=offering&offering_guid=d1aea7c0b17d4a56b6e03b75ae50f8dd&random=5fa0e8254dc62&iframeid=&mode=p", // html body
+    });
+    console.log("Message sent: %s", info.messageId);
+  }
+}
