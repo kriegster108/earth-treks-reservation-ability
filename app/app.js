c9fe81bf69e3a1fc2e46218dbc5a457d21960496
Hunter Krieg <kriegster108@gmail.com>
2020-11-02 20:39:40 -0700

    small changes

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const logic = new Logic();
 async function app() {
   const futureDate = logic.getSpecificWeekdayFutureDate(2);
   if (logic.isThereNoReservation(await request.postRequest(futureDate))) {
+    console.log(`Checking for date of: ${futureDate}:: reservation not ready`);
     wait(60000 * 15);
   } else {
     console.log(`opening found for ${futureDate} !!!!`);
--- app/requests/logic.js
+++ app/requests/logic.js
@@ -22,9 +22,8 @@ export default class Logic {
   }
   // 2
   getSpecificWeekdayFutureDate(numOfDayOfWeek) {
-    var d = new Date();
+    const d = new Date();
     d.setDate(d.getDate() + ((numOfDayOfWeek + 7 - d.getDay()) % 7));
-    console.log(d.toISOString().split("T")[0].trim());
     return d.toISOString().split("T")[0].trim();
   }
 }
